be98adb827043aad8fa1adb11cc39919b7907601
João Felipe Silva Lamounier <jjoaolamounier@gmail.com>
2023-12-18 17:07:42 -0300

    Excecao de atualizacao de pessoa

--- src/main/java/application/services/PessoaService.java
+++ src/main/java/application/services/PessoaService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,19 +32,23 @@ public class PessoaService {
     }
 
     public void delete(Integer id) {
-        try{
+        try {
             repository.deleteById(id);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
-        } catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             throw new DataBaseException(e.getMessage());
         }
     }
 
     public Pessoa update(Integer id, Pessoa obj) {
-        Pessoa entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            Pessoa entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(Pessoa entity, Pessoa obj) {
