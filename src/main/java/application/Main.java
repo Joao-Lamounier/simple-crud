0f723493b32187682b2984f2df8bd2a7f384f356
João Felipe Silva Lamounier <jjoaolamounier@gmail.com>
2023-12-27 21:34:33 -0300

    Refatoracao do main

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -21,56 +21,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("simple_crud");
-//
-//        EntityManager em = emf.createEntityManager();
-//
-//
-//        List<Cidade> cidades = lerArquivoJson("/home/joao/Documentos/cidadesjson/cidades.json");
-//
-//        em.getTransaction().begin();
-//        for (Cidade cidade : cidades) {
-//            em.persist(cidade);
-//        }
-//        em.getTransaction().commit();
-//
-//        System.out.println("Pronto");
+
 
     }
-//    public static List<Cidade> lerArquivoJson(String caminhoArquivo) {
-//        List<Cidade> cidades = new ArrayList<>();
-//
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            JsonNode rootNode = objectMapper.readTree(new File(caminhoArquivo));
-//
-//            JsonNode estadosNode = rootNode.get("estados");
-//            if (estadosNode != null) {
-//                Iterator<JsonNode> estadosIterator = estadosNode.elements();
-//
-//                while (estadosIterator.hasNext()) {
-//                    JsonNode estadoNode = estadosIterator.next();
-//                    String siglaEstado = estadoNode.get("sigla").asText();
-//                    String nomeEstado = estadoNode.get("nome").asText();
-//
-//                    JsonNode cidadesNode = estadoNode.get("cidades");
-//                    if (cidadesNode != null) {
-//                        Iterator<JsonNode> cidadesIterator = cidadesNode.elements();
-//
-//                        while (cidadesIterator.hasNext()) {
-//                            String nomeCidade = cidadesIterator.next().asText();
-//                            Cidade cidade = new Cidade(null, nomeCidade, siglaEstado);
-//                            cidades.add(cidade);
-//                        }
-//                    }
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return cidades;
-//    }
+
 
 }
 
